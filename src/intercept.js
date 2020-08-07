fb4fb5fac57f1061fc55f619b7411f0cad25efde
mycomputer <mycomputer@mycomputers-MacBook-Pro.local>
2020-08-06 23:16:02 -0400

    better blocking logic

--- src/intercept.js
+++ src/intercept.js
@@ -1,6 +1,28 @@
+function customBlocks() {
+    return new Set([])
+}
+
+function isFavicon(url) {
+    const faviconOptions = ["favicon.ico", "favicon"]
+
+    if (customBlocks().has(url)) {
+        return true
+    }
+
+    // Get lastPath with any query parameters stripped
+    const lastPath = url.split('/').pop().split('?')[0]
+
+    if (lastPath.endsWith(".png") || lastPath.endsWith(".ico") || lastPath.endsWith(".svg")) {
+        return faviconOptions.includes(lastPath.split('-')[0].split('@')[0])
+    }
+
+    return false
+}
+
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
-    return {cancel: ["favicon.ico", "favicon"].includes(details.url.split('/').pop().split('?')[0].split('@')[0]) };
+    return {cancel: isFavicon(details.url) };
   },
   {urls: ["<all_urls>"]},
-  ["blocking"]);
+  ["blocking"]
+);
